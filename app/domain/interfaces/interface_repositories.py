0106f758ece3537e870583d258078d4220c70f3c
pahdik <vlad0072338@mail.ru>
2024-01-17 00:39:55 +0300

    fix repositories

--- app/domain/interfaces/interface_repositories.py
+++ app/domain/interfaces/interface_repositories.py
@@ -2,21 +2,20 @@ from abc import ABC, abstractmethod
 from app.infrastucture.models.postgres_models import OriginalUrl, ShortenedUrl
 
 
-class IOriginalUrlRepository(ABC):
+class IUrlRepository(ABC):
     @abstractmethod
-    async def add_one(self, model: OriginalUrl) -> OriginalUrl:
+    async def find_shortened_url(self, shortened_url: str) -> ShortenedUrl:
         pass
 
     @abstractmethod
-    async def find_one(self, original_url: str) -> OriginalUrl:
-        pass
-
-
-class IShortenedUrlRepository(ABC):
-    @abstractmethod
-    async def add_one(self, model: ShortenedUrl) -> ShortenedUrl:
+    async def find_original_url(self, original_url: str) -> OriginalUrl:
         pass
 
     @abstractmethod
-    async def find_one(self, shortened_url: str) -> ShortenedUrl:
+    async def add_shortened_add_original_urls(self, original_url: OriginalUrl,
+                                              shortened_url: ShortenedUrl) -> ShortenedUrl:
+        pass
+
+    @abstractmethod
+    async def add_shortened_url(self, shortened_url: ShortenedUrl) -> ShortenedUrl:
         pass
--- app/infrastucture/repositories/db.py
+++ app/infrastucture/repositories/db.py
@@ -1,12 +1,10 @@
-from abc import ABC, abstractmethod
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import joinedload
-
-from app.domain.interfaces.interface_repositories import IOriginalUrlRepository, IShortenedUrlRepository
-from app.infrastucture.models.postgres_models import OriginalUrl, ShortenedUrl, Base
+from app.domain.interfaces.interface_repositories import IUrlRepository
 from app.infrastucture.configs.configs import PostgresSettings
+from app.infrastucture.models.postgres_models import OriginalUrl, ShortenedUrl, Base
 
 settings = PostgresSettings()
 
@@ -19,48 +17,18 @@ engine = create_async_engine(
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
-class OriginalUrlRepository(IOriginalUrlRepository):
-
-    async def __aenter__(self):
-        self.session = async_session()
-        return self
-
-    async def __aexit__(self, exc_type, exc, tb):
-        if exc:
-            await self.session.rollback()
-
-        await self.session.close()
-
-    async def add_one(self, model: OriginalUrl) -> None:
-        self.session.add(model)
-        await self.session.commit()
-
-    async def find_one(self, original_url: str) -> OriginalUrl | None:
-        query = (
-            select(OriginalUrl)
-            .where(OriginalUrl.original_url == original_url)
-            .options(joinedload(OriginalUrl.shortened_urls))
-        )
-        result = await self.session.execute(query)
-
-        found_original_url = result.unique().scalars().one_or_none()
-        return found_original_url
-
-
-class ShortenedUrlRepository(IShortenedUrlRepository):
+class UrlRepository(IUrlRepository):
     async def __aenter__(self):
         self.session = async_session()
 
     async def __aexit__(self, exc_type, exc, tb):
         if exc:
             await self.session.rollback()
+        else:
+            await self.session.commit()
         await self.session.close()
 
-    async def add_one(self, model: ShortenedUrl) -> None:
-        self.session.add(model)
-        await self.session.commit()
-
-    async def find_one(self, shortened_url: str) -> ShortenedUrl | None:
+    async def find_shortened_url(self, shortened_url: str) -> ShortenedUrl:
         query = (
             select(ShortenedUrl)
             .options(joinedload(ShortenedUrl.original_url, innerjoin=True))
@@ -68,3 +36,24 @@ class ShortenedUrlRepository(IShortenedUrlRepository):
         )
         result = await self.session.execute(query)
         return result.unique().scalars().one_or_none()
+
+    async def find_original_url(self, original_url: str) -> OriginalUrl:
+        query = (
+            select(OriginalUrl)
+            .where(OriginalUrl.original_url == original_url)
+            .options(joinedload(OriginalUrl.shortened_urls))
+        )
+        result = await self.session.execute(query)
+
+        return result.unique().scalars().one_or_none()
+
+    async def add_shortened_add_original_urls(self, original_url: OriginalUrl,
+                                              shortened_url: ShortenedUrl) -> ShortenedUrl:
+        shortened_url.original_url = original_url
+        self.session.add(original_url)
+        self.session.add(shortened_url)
+        return shortened_url
+
+    async def add_shortened_url(self, shortened_url: ShortenedUrl) -> ShortenedUrl:
+        self.session.add(shortened_url)
+        return shortened_url
